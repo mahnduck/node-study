f8b8f9887556cef0e7bd49ad367c74ec872fc31a
mahnduck Im <mahnduck@MD-MBP.local>
2018-05-13 15:10:06 +0900

    -

---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require ("http");
+
+function start() {
+    function onRequest(request, response){
+        console.log("Request is received.");
+        response.writeHead(200,{"Content-Type":"text/plain"});
+        response.write("Hello, mahnduck");
+        response.end();
+    }    
+
+    http.createServer(onRequest).listen(8888);
+    console.log("Server started.");
+}
+
+exports.start = start;
